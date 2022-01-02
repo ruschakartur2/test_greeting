596dbbeb5330aea3ca99a26b9a523086d70dfef6
ruschakartur2 <ruschakartur@gmail.com>
2022-01-02 13:08:41 +0200

    Add Gravatar library with avatars

--- front/src/components/GreetsList.js
+++ front/src/components/GreetsList.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Link} from "react-router-dom";
+import Gravatar from 'react-gravatar'
 
 const GreetsList = () => {
     const [greetedList, setGreetedList] = useState([]);
@@ -20,7 +21,7 @@ const GreetsList = () => {
         <div className={"greet-list"}>
             <h2>Бажаєш привітатись? <br/> <Link to={"/"}>Клікай</Link></h2> <br/>
             {greetedList && (<h3>З нами вже привітались: </h3>) && greetedList.length >= 1 ? greetedList.map((name, key)=>(
-                <p key={key}>{name}</p>
+                <p key={key}><Gravatar email={name+"@gmail.com"} /> {name}</p>
             )): (<h4>{error}</h4>)}
         </div>
     );
